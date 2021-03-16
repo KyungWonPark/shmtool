62c0c8971ec4c8f6343f7a2ee8991aa8d539cd9a
KyungWonPark <kw.park.person@gmail.com>
2021-03-16 14:47:08 +0900

    asdf

--- shm/shm.h
+++ shm/shm.h
@@ -8,7 +8,7 @@
 
 #define IPC_KEY_PROJID 0x42
 
-int sysv_shm_open(unsigned long size, int flags, int perm);
+int sysv_shm_open(unsigned long long size, int flags, int perm);
 void *sysv_shm_attach(int shm_id);
 int sysv_shm_detach(void *addr);
 int sysv_shm_write(int shm_id, void* input, int len, int offset);
